5035903a4c6206f1f972e9aa7eb090ab515dd378
Gezahegn Tsegaye <gezut38@gmail.com>
2025-03-29 09:37:44 +0100

    modified on payment methods

--- src/main/java/com/bin/pos/dal/model/PaymentMethod.java
+++ src/main/java/com/bin/pos/dal/model/PaymentMethod.java
@@ -1,12 +1,13 @@
 package com.bin.pos.dal.model;
-
 public enum PaymentMethod {
     CASH,
     CREDIT_CARD,
     DEBIT_CARD,
-    MOBILE_PAYMENT,
     BANK_TRANSFER,
+    MOBILE_PAYMENT,
     CHECK,
     STORE_CREDIT,
-    GIFT_CARD
+    GIFT_CARD,
+    PENDING,
+    OTHER
 }
--- src/main/java/com/bin/pos/dal/model/PaymentTransaction.java
+++ src/main/java/com/bin/pos/dal/model/PaymentTransaction.java
@@ -20,7 +20,7 @@ public class PaymentTransaction {
 
     private String paymentId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sales_transaction_id")
     private SalesTransaction salesTransaction;
 
